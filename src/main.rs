29f9a95449b449b966de9f403b1e4662ab062d5a
glitchy <lukas.r.jorgensen@gmail.com>
2022-11-04 09:55:08 -0400

    moving README to comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+// Memory layout of the f32 type in Rust (binary32) showing the three distinct
+// components encoded within the bits of a floating-point number for the f32 type.
+// '''
+// 
+// byte 0           byte 1           byte 2           byte 3
+// [][][][][][][][] [][][][][][][][] [][][][][][][][] [][][][][][][][]
+// |||               ||                                              |
+// | |               ||                                              |
+// | |               ||                                              |
+// | |_______________||______________________________________________|
+// |         |                                 |
+// |__,   exponent                          mantissa
+//    |
+// sign bit
+// '''
+
 const BIAS: i32 = 127;
 const RADIX: f32 = 2.0;
 
